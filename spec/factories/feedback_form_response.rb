5faa35f69f920e3b40ce9192327c0edd0aad08dd
Pranjal Rajput <pranjal.rajput.che21@itbhu.ac.in>
2024-07-02 02:07:10 +0530

    test

--- spec/factories/feedback_form_response.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :feedback_form_response do
-  end
-end
--- spec/factories/question_groups_factory.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :question_group, class: 'Rapidfire::QuestionGroup' do
-    name  { 'Survey' }
-    tags { '' }
-  end
-end
